4082037a90c91e18e310461c3af454a4ddbe071d
gurkay <g_basyigit@hotmail.com>
2019-03-08 23:51:39 +0300

    add text view component add edit text component

--- app/src/main/java/com/test/androidwidgets/MainActivity.java
+++ app/src/main/java/com/test/androidwidgets/MainActivity.java
@@ -2,7 +2,10 @@ package com.test.androidwidgets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,11 +18,39 @@ public class MainActivity extends AppCompatActivity {
     ///////////////////////////////////////////////////////////////
 
     TextView txtView;
+    EditText editText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///////////////////////////////////////////////////////////////
+        //
+        // Edit Text
+        //
+        ///////////////////////////////////////////////////////////////
+
+        editText = (EditText) findViewById(R.id.editTxtWriting);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = editText.getText().toString();
+                txtView.setText(str);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         ///////////////////////////////////////////////////////////////
         //
@@ -33,7 +64,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String myVar = txtView.getText().toString();
+
+                Toast.makeText(getApplicationContext(), myVar, Toast.LENGTH_SHORT).show();
+
             }
         });
+
     }
 }
